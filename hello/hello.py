7b929e2b14d66edab2a1a80fb6e3ace93a5c7a63
Lars Baekgaard <lars_baekgaard@yahoo.dk>
2018-06-05 13:51:47 +0000

    First working example using HTTP GET.

--- hello/hello.py
+++ hello/hello.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def test():
+    d = request.args
+
+    output_string = str(d)
+    try:
+        output_string += ".\n\nThe device is: " + d['device'] + "\nThe latitude is: " + d['lat'] + "\nThe longitude is: " + d['lon']
+    except KeyError:
+        pass
+
+    return output_string
